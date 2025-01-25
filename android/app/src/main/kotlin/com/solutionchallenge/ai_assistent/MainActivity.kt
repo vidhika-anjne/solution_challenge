77e5b20425a1e73924324574ee394d2f29e1124c
Vidhika' Anjne <anjnevidhika@gmail.com>
2025-01-25 20:57:45 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/solutionchallenge/ai_assistent/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.solutionchallenge.ai_assistent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+void main() {
+  // runApp(const MyHomePage());
+
+  runApp(MaterialApp(
+    home: MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String stringOutput = "Text Output";
+  final TextEditingController _textController = TextEditingController();
+
+  void geminiOutput() async {
+
+    if(_textController.text.isEmpty){
+      return;
+    }
+    final content = [Content.text(_textController.text)];
+    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: "AIzaSyB5Scfwg7YUuBVwsqJpJCkbVbCEv7pHCv8");
+    final response = await model.generateContent(content);
+    print(response.text);
+
+    setState(() {
+      stringOutput = response.text!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+                child: SingleChildScrollView(
+                  child: Text(
+                    stringOutput,
+                  ),
+                )
+              ),
+
+            TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                hintText:  "Enter text",
+              ),
+              onChanged: (value) {
+                setState(() {
+                  print(_textController.text);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: geminiOutput,
+        child: Text("Gemini Api"),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
